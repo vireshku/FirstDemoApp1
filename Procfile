62243c3dc313003fec4dd4b3327567c6e2c74ba4
viresh <virkumar@microsoft.com>
2020-06-23 14:23:28 +0530

    First demo app developement

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar target/FirstDemoApp1-0.0.1-SNAPSHOT.jar
---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,42 @@
+package com.example;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Map;
+
+import javax.measure.quantity.Mass;
+import javax.sql.DataSource;
+
+import org.jscience.physics.amount.Amount;
+import org.jscience.physics.model.RelativisticModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import static javax.measure.unit.SI.KILOGRAM;
+
+@Controller
+@SpringBootApplication
+public class Main {
+
+	@Value("${spring.datasource.url}")
+	private String dbUrl;
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Main.class, args);
+	}
+
+	@RequestMapping("/")
+	String index() {
+		return "index";
+	}
+}
